80ed77a1ba81891c08c94c51e97e0d7fa395e0a6
Peter <pietrociccarello@gmail.com>
2018-07-29 15:02:35 +0200

    Add new style to buttons in admin page

--- view/back_office/admin/admin.php
+++ view/back_office/admin/admin.php
@@ -1,50 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8" />
 	<title>Admin</title>
-	<link href="../public/css/stylesheet.css" rel="stylesheet" />
-    <script src="tinymce/tinymce.js" language="javascript" type="text/javascript">
-    </script>
-    <script language="javascript" type="text/javascript">
-        tinyMCE.init({
-        mode : "textareas"
-        });
-    </script>
 </head>
 <body>
-	<h1>Page de l'Admin</h1>
-	<a id="logout" href='index.php?action=adminUserDeconnect'>Deconnexion</a> 
-    <br />
-    <br />
-    <a id="logout" href="index.php?action=adminAddPost">Ajouter un nouveau post</a>
-    <br />
-    <br />
-    <a id="commentaires_signalés" href="index.php?action=adminShowReportedComment">Commentaires Signalés</a>
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('public/thème/img/writer.jpg')">
+            <div class="site-heading">
+              <h1>Page de l'Admin</h1>
+              <span class="subheading">Ici vous pouvez ajouter, modifier ou supprimer des billets</span>
+            </div>
+    </header>
 
 <?php ob_start(); ?>
-<h1>Ma Page Admin !</h1>
-<p> Tous les billets du blog :</p>
-
-<em><a href="../../index.php?action=all">voir tous les post</a></em>
-
-<div><img alt="writer" src="http://www.lorenzomarone.net/wp-content/uploads/2014/12/scrittore-981x540.jpg"></div>
 
 <?php
 foreach($all as $data) //foreach($all as $data);
 //foreach bouche like while but it works only on arrays,  raccoglie tutti i dati e li dispaccia
 {
 ?>
-    <div class="news">
-        <h3>
-            <?= htmlspecialchars($data['title']) ?>
-            <em>le <?= $data['creation_date_fr'] ?></em>
-        </h3>
-        
-        <p>
-            <?= strip_tags($data['content'], '<br> <em> <strong>')?>
-
-            <!-- faire une fonction pour le strip_tags et le mettre dans toute les vues-->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-preview">
+            <a href="index.php?action=adminPost&amp;id=<?= $data['id'] ?>">
+              <h2 class="post-title">
+                <?= ($data['title']) ?>
+                </h2>
+              <h3 class="post-subtitle">
+                <?= strip_tags(substr($data['content'], 0, 100), '<br> <em> <strong>' ) ?>...
+                      <!-- faire une fonction pour le strip_tags et le mettre dans toute les vues-->
 
             <!-- function stripTags($string){
 
@@ -54,18 +39,30 @@ foreach($all as $data) //foreach($all as $data);
                   fare un fichier utilities.php oà mettre tous les fonctions et l'inclure dans index.php
 
                   l'appeler comme ça stripTags($data['content'])  au lieu de  strip_tags($data['content'], '<br> <em>' ecc)} -->
+              </h3>
+            </a>
+            <p class="post-meta"> le <?= $data['creation_date_fr'] ?>
             <br />
-            <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Commentaires</a></em>
-            <em><a href="index.php?action=adminEditPost&amp;id=<?= $data['id'] ?>">Editer le billet</a></em>
-            <em><a href="index.php?action=adminRemovePost&amp;id=<?= $data['id']?>">Supprimer le billet</a></em>
-        </p>
+  
+            </p>
+              
+            <a style="text-decoration: none" href="index.php?action=adminPost&amp;id=<?= $data['id'] ?>"><input type="button" class="btn" value="Voir le post" /></a>
+            <a style="text-decoration: none" href="index.php?action=adminEditPost&amp;id=<?= $data['id'] ?>"><input type="button" class="btn" value="Modifier le post" /></a>
+            <a style="text-decoration: none" href="index.php?action=adminRemovePost&amp;id=<?= $data['id']?>" onclick="return confirm('Voulez vous vraiment supprimer ce post?')"><input type="button" class="btn" value="Supprimer le post" /></a>
+        <br />
+        <br />
+        <br />
+          </div>
+        </div>
     </div>
+</div>
+
 <?php
 }
 ?>
 <?php $content = ob_get_clean(); ?>
 
-<?php require(FRONT_OFFICE_VIEW_PATH . 'template.php'); ?>
+<?php require(ADMIN_VIEW_PATH . 'templateAdmin.php'); ?>
 
 
 	</body>\
