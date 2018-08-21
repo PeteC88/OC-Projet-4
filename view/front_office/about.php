0384b1e7d550c1c3b6c0b33d2c216792ff3c0027
Peter <pietrociccarello@gmail.com>
2018-08-21 23:15:59 +0200

    added new image in about page

--- view/front_office/about.php
+++ view/front_office/about.php
@@ -37,6 +37,9 @@
             <li class="nav-item">
               <a class="nav-link" href="../../index.php">Home</a>
             </li>
+              <li class="nav-item">
+              <a class="nav-link" href="../../index.php?action=about">About</a>
+            </li>
             <li class="nav-item">
               <a class="nav-link" href="../../index.php?action=userConnect">Connexion</a>
             </li>
@@ -61,7 +64,7 @@
     </header>
 
     <section>
-      <img src="../../public/thème/img/jean2.jpg" alt="Jean-Forteroche" style="padding-left: 12%;">
+      <img src="../../public/thème/img/jean2.jpg" alt="Jean-Forteroche" style="display: block; margin-left: auto; margin-right: auto;">
       <div class="container" style="padding-left: 10%; padding-right: 10%;">
         <p><strong>Lorem ipsum</strong> dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.</p>
 
