e94268e60ff1af605d4a658594f49304f4ecdebe
Peter <pietrociccarello@gmail.com>
2018-08-21 23:09:10 +0200

    Added redirection in front_office Controller if the id of the post doesn't exist

--- front_office/Controllers/PostController.php
+++ front_office/Controllers/PostController.php
@@ -3,7 +3,6 @@
 class PostController
 {
 	private $PostModel;
-	//private $CommentModel;
 
 	public function __construct()
 	{
@@ -11,27 +10,45 @@ class PostController
 	}
 
 
-	function listPosts()
+	public function listPosts()
 	{
 	    $post = $this->PostModel->getPosts(5);
 
 	    require(FRONT_OFFICE_VIEW_PATH . 'listPostsView.php');
 	}
 
-	function listAll()
+	public function listAll()
 	{
 		$all = $this->PostModel->getPosts();
 		
     	require(ADMIN_VIEW_PATH . 'admin.php');
 	}
 
-	function post()
+	public function post()
 	{	
-    	$post = $this->PostModel->getPost($_GET['id']);
-    	$comments = $this->PostModel->getPostComments($_GET['id']);
+    	$post = $this->PostModel->getPost($_GET['id']);;
+    	
+    	if(!empty($post))
+    	{
+    		
+    		$comments = $this->PostModel->getPostComments($_GET['id']);
 
-    	require(FRONT_OFFICE_VIEW_PATH . 'postView.php');
+    		require(FRONT_OFFICE_VIEW_PATH . 'postView.php');
+	
+    	}
+    	else
+    	{
+
+    		require('view/front_office/404.html');
+
+    	}
+    	
 	}
+	
+	public function about()
+	{
+		require('view/front_office/about.php');
+	} 
 
 }
 
