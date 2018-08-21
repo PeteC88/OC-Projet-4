9d832f833be853a657f9268718f4c70f90460bea
Peter <pietrociccarello@gmail.com>
2018-08-21 23:40:29 +0200

    Added function to add a preview in Utility and comments delted

--- lib/utility.php
+++ lib/utility.php
@@ -1,22 +1,18 @@
 <?php
-class utility
+
+function stripTags($string)
 {
-	public function stripTags($string)
-	{
-		return strip_tags($data['content'], '<br><em><strong><i><b><p><div><ul><ol><li><section><content><span><h1><h2><h3><h4><h5><h6><body><html><title><abbr><form><input><textarea>')
-	}
-
-	public function()
-	
+  //to delete the html and php tags in a string
+	return strip_tags($string ,'<br><em><strong><i><b><p><div><ul><ol><li><section><content><span><h1><h2><h3><h4><h5><h6><body><html><title><abbr><form><input><textarea>');
 }
-/*
-<!-- faire une fonction pour le strip_tags et le mettre dans toute les vues-->
 
-            <!-- function stripTags($string){
 
-                return strip_tags($data['content'], '<br><em><a><p><div><strong>')
-                
+//Function to make a text preview in home page
 
-                  fare un fichier utilities.php oà mettre tous les fonctions et l'inclure dans index.php
+function createPreview($text, $limit) 
+{
+  $text = preg_replace('/\[\/?(?:b|i|u|s|center|quote|url|ul|ol|list|li|\*|code|table|tr|th|td|youtube|gvideo|(?:(?:size|color|quote|name|url|img)[^\]]*))\]/', '', $text);
 
-                  l'appeler comme ça stripTags($data['content'])  au lieu de  strip_tags($data['content'], '<br> <em>' ecc)} -->*/
+  if (strlen($text) > $limit) return substr($text, 0, $limit) . "...";
+  return $text;
+}
