aab89e9c3d1cb1e1193c784cb10e0c34b0164e1f
Peter <pietrociccarello@gmail.com>
2018-08-21 23:11:26 +0200

    Some comments deleted in CommentController.php in Front_office

--- front_office/Controllers/CommentController.php
+++ front_office/Controllers/CommentController.php
@@ -13,33 +13,28 @@ class CommentController
 	{
 	    $affectedLines = $this->CommentModel->postComment($postId, $author, $comment);
 
-	    if ($affectedLines === false) {
-	        //Erreur gérée. Elle sera remontée jusqu'au bloc try du routeur !
+	    if ($affectedLines === false) 
+	    {
 	        throw new Exception('Impossible d\'ajouter le commentaire !');
 	    }
 	    else {
 	        header('Location: index.php?action=post&id=' . $postId);
+	        exit();
 	    }
 	}
 	
 	function reportCommentAction()
 	{
-	    //au clic du bouton signaler (changer dans la bdd le champs reported comment e le mettre à 1)
-	    //recupere l'id du commentaire à modifier (depuis le name du champ du formulaire)
 	    $commentId = $_POST['commentId'];
 	    $postId = $_POST['postId'];
 
-	    //modifier l'information en base de donnée
-	    //mettre la valeur directe comme paramètre à la place de l'attribut $reportedComment
-	    //DONNER DES VALEURS AUX PARAMETRES
-	    //$commentId comme attribut pour recuperer l'id du commentaire:
+	  
+	    //when you click on the button it will modify the value reported comment of the db.
 	    $this->CommentModel->reportComment(1, $commentId);
-
-	    //ecrire un petit message pour dire que le commentaire a bien été signalé.
-
 
 
 	    header('Location: index.php?action=post&id=' . $postId);
+	    exit();
 	}
 
 	function showReportedComment()
